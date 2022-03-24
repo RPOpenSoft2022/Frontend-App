0ebab3cc0878555517c3847f36f7d7f750eaa490
aayush airan <ayushairan799@gmail.com>
2022-03-24 17:25:26 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import React from "react";
 import { Routes } from "react-router-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LogIn from "./components/LogIn/LogIn";
 import Register from "./components/Register/Register";
+import Stores from "./components/Stores/Stores";
+import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
+import { CartProvider } from "./Contexts/CartContext";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LogIn />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Stores" element={<Stores />} />
-        </Routes>
-      </Router>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LogIn />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Stores" element={<Stores />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/Checkout" element={<Checkout />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </div>
   );
 }

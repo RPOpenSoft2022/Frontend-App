ab047b77de2694036fc199c23782db30657f65c4
aayush airan <ayushairan799@gmail.com>
2022-03-27 00:56:32 +0530

    .

--- src/Contexts/cart.js
+++ src/Contexts/cart.js
@@ -6,38 +6,40 @@ export const cartData = {
       name: "chicken",
       quantity: "5",
       price: "50",
-      thumbnail: "https://thecurrymommy.com/wp-content/uploads/2021/09/butter-paneer-masala-one-pot.jpg"
+      thumbnail:
+        "https://thecurrymommy.com/wp-content/uploads/2021/09/butter-paneer-masala-one-pot.jpg",
     },
     {
       id: "2",
       name: "butter",
       quantity: "4",
       price: "50",
-      thumbnail: "https://thecurrymommy.com/wp-content/uploads/2021/09/butter-paneer-masala-one-pot.jpg"
+      thumbnail:
+        "https://thecurrymommy.com/wp-content/uploads/2021/09/butter-paneer-masala-one-pot.jpg",
     },
     {
       id: "3",
       name: "bread",
       quantity: "10",
-      price: "50"
+      price: "50",
     },
     {
       id: "4",
       name: "milk",
       quantity: "1",
-      price: "50"
+      price: "50",
     },
     {
       id: "5",
       name: "liquor",
       quantity: "6",
-      price: "50"
+      price: "50",
     },
     {
       id: "6",
       name: "JamRoll",
       quantity: "6",
-      price: "50"
+      price: "50",
     },
   ],
 };
--- src/components/Register/Verification.js
+++ src/components/Register/Verification.js
@@ -1,120 +1,92 @@
-import React from "react";
+import React, { Component } from "react";
 import OtpInput from "react-otp-input";
 import LockIcon from "@mui/icons-material/Lock";
-import { makeStyles } from "@mui/styles";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import {
-  CssBaseline,
-  Avatar,
-  Button,
-  Grid,
-  Paper,
-  Container,
-  Typography,
-} from "@mui/material";
+import { Container, Box, Grid, Typography, Button } from "@mui/material";
+export default class Verification extends Component {
+  state = { otp: "" };
 
-// const theme = createTheme();
-// const useStyles = makeStyles((theme) => ({
-//   grid: {
-//     backgroundColor: "grey",
-//     height: "50vh",
-//     textAlign: "center",
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   submit: {
-//     margin: theme.spacing(3),
-//   },
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//   },
-// }));
+  handleChange = (otp) => this.setState({ otp });
 
-export default function Verification() {
-  // const classes = useStyles();
-  return (
-    <Container
-      component="main"
-      maxWidth="sm"
-      style={{
-        backgroundColor: "red",
-      }}
-    >
-      <CssBaseline />
-      <div>
-        <Grid
-          container
-          style={{ backgroundColor: "white" }}
-          justify="center"
-          alignItems="center"
-          spacing={3}
+  render() {
+    return (
+      <>
+        <Container
+          style={{
+            minWidth: "300px",
+            maxWidth: "50%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "1rem",
+            backgroundColor: "white",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
         >
-          aayush
-          <Grid item container justify="center">
-            <Grid item container alignItems="center" direction="column">
-              <Grid item>
-                <Avatar>
-                  <LockIcon />
-                </Avatar>
-              </Grid>
-              <Grid item>
-                <Typography component="h1" variant="h5">
-                  Verification Code
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} textAlign="center">
-            <Paper elevation={0}>
-              <Typography variant="h6">
-                Please enter the verification code sent to your mobile
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            container
-            justify="center"
-            alignItems="center"
-            direction="column"
+          <LockIcon color="primary" fontSize="large" />
+          <Typography
+            variant="h5"
+            align="center"
+            style={{
+              margin: "1rem",
+            }}
           >
-            <Grid item spacing={3} justify="center">
-              <OtpInput
-                separator={
-                  <span>
-                    <strong>:</strong>
-                  </span>
-                }
-                inputStyle={{
-                  width: "3rem",
-                  height: "3rem",
-                  margin: "0 0.5rem",
-                  fontSize: "2rem",
-                  color: "black",
-                  borderRadius: 4,
-                  border: "1px solid rgba(0,0,0,0.3)",
-                }}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Verify
-              </Button>
-            </Grid>
+            Verification Code
+          </Typography>
+          <Typography
+            variant="h6"
+            align="center"
+            style={{
+              fontSize: "1rem",
+              margin: "1rem",
+            }}
+          >
+            Please Enter The Verification Code Sent To Your Mobile
+          </Typography>
+          <Grid
+            justify="center"
+            style={{
+              width: "300px",
+              margin: "0 auto",
+            }}
+          >
+            <OtpInput
+              value={this.state.otp}
+              onChange={this.handleChange}
+              numInputs={4}
+              separator={<span>:</span>}
+              inputStyle={{
+                position: "relative",
+                display: "block",
+                width: "3rem",
+                height: "3rem",
+                margin: "0 0.5rem",
+                fontSize: "2rem",
+                color: "black",
+                borderRadius: 4,
+                border: "1px solid rgba(0,0,0,0.3)",
+              }}
+              containerStyle="div"
+              isInputNum
+            />
           </Grid>
-        </Grid>
-      </div>
-    </Container>
-  );
+          <Button
+            variant="contained"
+            type="submit"
+            style={{
+              marginTop: "10px",
+            }}
+            onClick={() => {
+              console.log(this.state.otp);
+            }}
+          >
+            Verify
+          </Button>
+        </Container>
+      </>
+    );
+  }
 }

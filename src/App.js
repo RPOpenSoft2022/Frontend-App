187febe3135eaa0faa7d9b54e56989d26a6cfa7a
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2022-03-26 15:01:03 +0530

    added userprovider in app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Stores from "./components/Stores/Stores";
 import Cart from "./components/Cart/Cart";
 import Checkout from "./components/Checkout/Checkout";
 import { CartProvider } from "./Contexts/CartContext";
+import { UserProvider } from "./Contexts/UserContext";
 import "./App.css";
 import Store from "./components/Store/Store";
 import Orders from "./components/Orders/Orders";
@@ -17,22 +18,24 @@ import Profile from "./components/Profile/Profile";
 function App() {
   return (
     <div className="App">
-      <CartProvider>
-        <Router>
-          <ResponsiveAppBar />
-          <Routes>
-            <Route path="/" element={<LogIn />} />
-            <Route path="/Register" element={<Register />} />
-            <Route path="/Stores" element={<Stores />} />
-            <Route path="/Stores/:id" element={<Store />} />
-            <Route path="/Orders" element={<Orders />} />
-            <Route path="/Orders/:id" element={<Order />} />
-            <Route path="/Cart" element={<Cart />} />
-            <Route path="/Checkout" element={<Checkout />} />
-            <Route exact path="/Profile" element={<Profile />} />
-          </Routes>
-        </Router>
-      </CartProvider>
+      <UserProvider>
+        <CartProvider>
+          <Router>
+            <ResponsiveAppBar />
+            <Routes>
+              <Route path="/" element={<LogIn />} />
+              <Route path="/Register" element={<Register />} />
+              <Route path="/Stores" element={<Stores />} />
+              <Route path="/Stores/:id" element={<Store />} />
+              <Route path="/Orders" element={<Orders />} />
+              <Route path="/Orders/:id" element={<Order />} />
+              <Route path="/Cart" element={<Cart />} />
+              <Route path="/Checkout" element={<Checkout />} />
+              <Route exact path="/Profile" element={<Profile />} />
+            </Routes>
+          </Router>
+        </CartProvider>
+      </UserProvider>
     </div>
   );
 }

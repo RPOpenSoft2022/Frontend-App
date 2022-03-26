076a700de3527f635616633321c034c45a6ef05a
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2022-03-26 18:34:15 +0530

    added functions in payment

--- src/Contexts/User.js
+++ src/Contexts/User.js
@@ -1,4 +1,9 @@
 export const userData = {
-  username: "Guest User",
+  first_name: 'Shobhit',
+  middle_name: '',
+  last_name: 'Gupta',
+  address :'User address',
+  email: 'user@example.com',
+  phone: '7260434343',
   token: "No Token",
 };
